3e6e2b37df1ce91aecedc0d543b2b2a019b0d40a
chihulloli123 <chihulloli123@gmail.com>
2024-12-14 05:59:19 -0600

    Update newFile.cpp

--- newFile.cpp
+++ newFile.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
-#include <vector>
-#include <iomanip>
+#include <fstream>
+#include <cmath>
 #include <algorithm>
-#inlcude <fstream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main() {
-	
-	double myOutputList;
-	ifstream myReadFile("input.txt");
 
-	while (getline (myReadFile, myOutputList)) {
+	//vector<string> myOutputList;
+	vector<int> myLeftList;
+	vector<int> myRightList;
+
+	//string rightline;
+	//string leftline;
 	
-	cout << myOutputList;
-	
+	ifstream myReadFile("input.txt");
+	if (!myReadFile) {
+		cerr << "Error opening file!" << endl;
+		return 1;
 	}
 
-	vector<double> leftList(myOutputList);
-	vector<double> rightList(myOutputList);
+	string sLine;
+	int lineNumber = 0;
+	while (getline(myReadFile, sLine)) {
+		lineNumber++;
+		int left, right;
+		if (sscanf(sLine.c_str(), "%d %d" , &left, &right) == 2) {
+		myLeftList.push_back(left);
+		myRightList.push_back(right);
+	} else {
+			cerr << "Wrong" << lineNumber << ": " << sLine << endl;
+			return 1;
+		}
+		//myOutputList.push_back(line);
+	}
+	
+	myReadFile.close();	
+ 
+	sort(myLeftList.begin(), myLeftList.end());
+	sort(myRightList.begin(), myRightList.end());
 
-	//do some kind of calculation 
-
-	//spit out result
-
-	myReadFile.close();
-}
+	int dayOneAnswer = 0;
+	for (size_t i = 0; i < myLeftList.size(); i++) {
+		dayOneAnswer  += abs(myLeftList[i] - myRightList[i]);
+	}
+	
+	cout << "Day 1 Answer: " << dayOneAnswer << endl;
+	
+	return 0;
